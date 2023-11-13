6f3ca069666ae985d7a193f6791fea4d0594b1de
ศักรินทร์ สิงห์อยู่ <sekkri1234@gmail.com>
2023-11-13 15:41:02 +0700

    first commit

---
+++ src/BMICalculator.ts
@@ -0,0 +1,13 @@
+// src/BMICalculator.ts
+class BMICalculator {
+    calculateBMI(weightInKg: number, heightInMeters: number): number {
+      if (heightInMeters <= 0 || weightInKg <= 0) {
+        throw new Error("Invalid input. Weight and height must be positive values.");
+      }
+  
+      const bmi: number = weightInKg / (heightInMeters * heightInMeters);
+      return bmi;
+    }
+  }
+  
+  export default BMICalculator;
